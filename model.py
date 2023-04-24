51d2eb570af7aef0bfb71127cb78b476aa0fc577
Ali Zarghami <alizarghami@gmail.com>
2023-04-23 22:35:03 -0700

    [FIX] Add actions to second layer

--- model.py
+++ model.py
@@ -17,16 +17,16 @@ class Critic(nn.Module):
         """
         super(Critic, self).__init__()
         self.seed = torch.manual_seed(seed)
-        self.fc1 = nn.Linear(state_size + action_size, fc1_units)
+        self.fc1 = nn.Linear(state_size, fc1_units)
         self.bn1 = nn.BatchNorm1d(fc1_units)
-        self.fc2 = nn.Linear(fc1_units, fc2_units)
+        self.fc2 = nn.Linear(fc1_units + action_size, fc2_units)
         self.fc3 = nn.Linear(fc2_units, 1)
         self.reset_parameters()
 
     def forward(self, state, action):
         """Build a network that maps state -> action values."""
-        state_action = torch.cat((state, action), dim=1)
-        x = self.bn1(F.relu(self.fc1(state_action)))
+        x = self.bn1(F.relu(self.fc1(state)))
+        x = torch.cat((x, action), dim=1)
         x = F.relu(self.fc2(x))
         return self.fc3(x)
 
